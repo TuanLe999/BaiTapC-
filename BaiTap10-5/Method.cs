9e40c30dd3c4dd491b851e40cd3524f3f9d29729
TuanLe999 <99901063+TuanLe999@users.noreply.github.com>
2022-05-18 01:19:04 +0700

    Bai tap ngay 10 - 5

---
+++ BaiTap10-5/Method.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap10_5
+{
+    class Method
+
+    {
+        public int[] createArray(int[] a, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Enter the number " + i + ": ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            return a;
+        }
+        public int max(int[] a, int n)
+        {
+            int m = a[1];
+            for (int i = 1; i <= n; i++)
+            {
+                if (a[i] > m)
+                    m = a[i];
+            }
+            return m;
+        }
+
+        public int min(int[] a, int n)
+        {
+            int m = a[1];
+            for (int i = 1; i <= n; i++)
+            {
+                if (a[i] < m)
+                    m = a[i];
+            }
+            return m;
+        }
+
+        public int count(int[] a, int n, int b)
+        {
+            int c = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (a[i] == b)
+                    c = c + 1;
+            }
+            return c;
+        }
+
+        public int maxCount(int[] a, int n)
+        {
+            int m = count(a, n, a[1]);
+            int b = a[1];
+            for (int i = 1; i <= n; i++)
+            {
+                if (count(a, n, a[i]) > m)
+                {
+                    m = count(a, n, a[i]);
+                    b = a[i];
+                }
+            }
+            return b;
+        }
+
+        public void softMinMax(ref int[] a, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i + 1; j <= n; j++)
+                {
+                    if (a[j] < a[i])
+                    {
+                        int temp = a[j];
+                        a[j] = a[i];
+                        a[i] = temp;
+                    }
+                }
+            }
+        }
+
+        public void softMaxMin(ref int[] a, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i + 1; j <= n; j++)
+                {
+                    if (a[j] > a[i])
+                    {
+                        int temp = a[j];
+                        a[j] = a[i];
+                        a[i] = temp;
+                    }
+                }
+            }
+        }
+
+        public void display(int[] a, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(a[i] + "  ");
+            }
+            Console.WriteLine("");
+        }
+
+    }
+}
---
+++ BaiTap10-5/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap10_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method method = new Method();
+            int []a = new int[11];
+            a = method.createArray(a, 10);
+            Console.Write("Aray: ");
+            method.display(a, 10);
+            Console.WriteLine("The max number is: " + method.max(a,10));
+            Console.WriteLine("The min number is: " + method.min(a,10));
+            Console.WriteLine("The number with the highest frequency is: " + method.maxCount(a,10));
+            Console.WriteLine("The array when sorted in ascending order is: ");
+            method.softMinMax(ref a, 10);
+            method.display(a, 10);
+            Console.WriteLine("The array when sorted in descending order is: ");
+            method.softMaxMin(ref a, 10);
+            method.display(a, 10);
+
+            Console.ReadKey();
+
+        }
+    }
+}
